de4e012b33dbfeceb74cf1bc2adc59be7925e328
Rafael Souza <rafasouza2509@gmail.com>
2020-07-03 11:13:32 -0300

    finished page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return <h1>Hello World!</h1>;
-};
+import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
